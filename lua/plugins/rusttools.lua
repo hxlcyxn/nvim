8f1f76986285fcd111ca209c6dd277e162274508
JoPro0406 <55317573+JoPro0406@users.noreply.github.com>
2021-10-13 18:52:20 +0200

    fix(rusttools): remove unneded config

--- lua/plugins/rusttools.lua
+++ lua/plugins/rusttools.lua
@@ -1,9 +1,5 @@
 require("rust-tools").setup {
   tools = {
-    inlay_hints = {
-      show_parameter_hints = true,
-      parameter_hints_prefix = "ᐊ",
-      other_hints_prefix = "»",
-    },
+    inlay_hints = { parameter_hints_prefix = "ᐊ", other_hints_prefix = "»" },
   },
 }
