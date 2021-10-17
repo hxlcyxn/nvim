01a9b25931107a82957fd40e788fe111d1860926
JoPro0406 <55317573+JoPro0406@users.noreply.github.com>
2021-10-17 19:02:23 +0200

    fix(nvim-tree): change to new config format

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,20 +1,6 @@
 -- https://github.com/kyazdani42/nvim-tree.lua/
-vim.g.nvim_tree_side = "left"
-vim.g.nvim_tree_width = 25
-vim.g.nvim_tree_ignore = { ".git", "node_modules" }
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_ignore_ft = { "dashboard" }
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_quit_on_open = 0
-vim.g.nvim_tree_follow = 0
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_git_hl = 0
-vim.g.nvim_tree_root_folder_modifier = ":~"
-vim.g.nvim_tree_tab_open = 0
-vim.g.nvim_tree_width_allow_resize = 0
-vim.g.nvim_tree_disable_netrw = 1
-vim.g.nvim_tree_hijack_netrw = 1
-vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_group_empty = 1
+require("nvim-tree").setup {
+  hijack_cursor = true,
+  update_cwd = true,
+  auto_resize = true,
+}
