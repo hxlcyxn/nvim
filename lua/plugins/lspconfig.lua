ad72e23fd95d579f32bb668336a1f93a4d6ceae4
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2022-10-17 10:10:40 +0200

    fix: cmp-nvim-lsp changed capabilies handling

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -51,8 +51,7 @@ local function on_attach(client, bufnr)
 	end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local function init_lsp(name, extra_config)
 	lspconfig[name].setup(
