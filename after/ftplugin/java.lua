62193867dad83b548a087b53e4460d8483f44556
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2022-10-28 13:38:26 +0200

    feat: simplify java config

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -2,37 +2,11 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = vim.fn.stdpath("data") .. "/jdtls/" .. project_name
 
 local config = {
-	cmd = { "jdtls", "--jvm-arg=-Dlog.level=ALL", "-data", workspace_dir},
-	-- cmd = {
-	-- 	"java",
-
-	-- 	"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-	-- 	"-Dosgi.bundles.defaultStartLevel=4",
-	-- 	"-Declipse.product=org.eclipse.jdt.ls.core.product",
-	-- 	"-Dlog.protocol=true",
-	-- 	"-Dlog.level=ALL",
-	-- 	"-Xms1g",
-	-- 	"--add-modules=ALL-SYSTEM",
-	-- 	"--add-opens",
-	-- 	"java.base/java.util=ALL-UNNAMED",
-	-- 	"--add-opens",
-	-- 	"java.base/java.lang=ALL-UNNAMED",
-
-	-- 	"-jar",
-	-- 	"/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
-
-	-- 	"-configuration",
-	-- 	"/usr/share/java/jdtls/config_linux",
-	-- 	"-data",
-	-- 	workspace_dir,
-	-- },
-
-	settings = {
-		java = {},
-	},
-
-	init_options = {
-		bundles = {},
-	},
+	cmd = { "jdtls", "-data", workspace_dir },
+	root_dir = vim.fs.dirname(vim.fs.find({
+		".gradlew",
+		".git",
+		"mvnw",
+	}, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
