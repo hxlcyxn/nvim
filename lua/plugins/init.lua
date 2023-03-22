80d6b147dc9f769086c8c9ba468da4269c32ebe5
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-03-22 21:07:32 +0100

    more yessir

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,13 @@
 return {
-	"folke/which-key.nvim",
 	{
 		"hxlcyxn/stargaze.nvim",
 		lazy = false,
 		config = function()
 			vim.cmd("colorscheme stargaze")
 		end,
+	},
+	{
+		"jghauser/mkdir.nvim",
+		event = { "BufWritePre" },
 	},
 }
