2e32345cc5958f6148f847514fdfdcf4538606f2
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-04-05 18:26:31 +0200

    add docs to util

--- lua/util.lua
+++ lua/util.lua
@@ -1,9 +1,15 @@
 local M = {}
 
+--- Returns the specified command as a formatted cmd-mapping.
+---@param c string
+---@return string
 function M.cmd(c)
 	return "<cmd>" .. c .. "<cr>"
 end
 
+--- Wraps the setup() call for the supplied function.
+---@param name string
+---@return function
 function M.wrapsetup(name)
 	return function(_, opts)
 		require(name).setup(opts)
