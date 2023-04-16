a960a11ce7d9edaa1412f1fd8b07c277d63301e2
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-04-16 18:54:50 +0200

    feat: change save keybind

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,7 @@ local map = vim.api.nvim_set_keymap
 vim.g.mapleader = Settings.mapleader
 vim.g.maplocalleader = Settings.maplocalleader
 
-map("n", "<leader>s", cmd("update"), { desc = "save changes" })
+map("n", "<leader>S", cmd("update"), { desc = "save changes" })
 map("n", "<leader>q", cmd("quit"), { desc = "quit nvim" })
 map("n", "<leader>bh", cmd("bprevious"), { desc = "previous buffer" })
 map("n", "<leader>bl", cmd("bnext"), { desc = "next buffer" })
