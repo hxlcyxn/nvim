ff6df9d7fcf8045a1c2c8ab8d0f0508797c6c9f9
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-04-23 01:12:08 +0200

    dressing changes

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -11,14 +11,14 @@ return {
 			telescope = {
 				layout_strategy = "horizontal",
 				layout_config = {
-					width = 0.2,
-					height = 0.3,
+					width = 0.5,
+					height = 0.4,
 				},
 				borderchars = Settings.borderchars,
 			},
 			get_config = function(opts)
 				if opts.kind == "codeaction" then
-					return { telescope = require("telescope.themes").get_cursor() }
+					return { telescope = require("telescope.themes").get_cursor({ borderchars = Settings.borderchars }) }
 				end
 			end,
 		},
