eb5f6cd6206435c17f4bfbefc54a68438cb7203a
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-10-14 19:46:31 +0200

    add nvim-treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,47 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	init = function()
-		vim.opt.foldmethod = "expr"
-		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-		vim.opt.foldlevel = 42
-	end,
-	opts = {
-		ensure_installed = "all",
-		ignore_install = {
-			"json",
-			"jsonc",
-			"comment",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		init = function()
+			vim.opt.foldmethod = "expr"
+			vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+			vim.opt.foldlevel = 42
+		end,
+		opts = {
+			ensure_installed = "all",
+			ignore_install = {
+				"json",
+				"jsonc",
+				"comment",
+			},
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+				additional_vim_regex_highlighting = true,
+			},
+			indent = { enable = true },
 		},
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-			additional_vim_regex_highlighting = true,
-		},
-		indent = { enable = true },
+		config = require("util").wrapsetup("nvim-treesitter.configs"),
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		depends = { "nvim-treesitter/nvim-treesitter" },
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+			},
+		},
+		config = require("util").wrapsetup("nvim-treesitter.configs"),
+	},
 }
