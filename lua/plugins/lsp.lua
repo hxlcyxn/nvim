bcfe9ccadb54a8c53c6d5f03fe738cf4773fcbfd
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-01-16 20:17:42 +0100

    fix inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,7 +64,7 @@ return {
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
 
 					if client.server_capabilities.inlayHintProvider then
-						vim.lsp.inlay_hint(buffer, true)
+						vim.lsp.inlay_hint.enable(buffer, true)
 					end
 				end,
 			})
