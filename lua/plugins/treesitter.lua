6d5b9e4dcde1cab627d569e9653713a8465b9f02
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-01-26 22:12:55 +0100

    dont use regex highlighting with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,7 +18,7 @@ return {
 			highlight = {
 				enable = true,
 				use_languagetree = true,
-				additional_vim_regex_highlighting = true,
+				additional_vim_regex_highlighting = false,
 			},
 			indent = { enable = true },
 			incremental_selection = {
