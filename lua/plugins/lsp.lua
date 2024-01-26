bf7f0d357d4d0e78c51309d329299f5d6b31e893
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-01-26 22:13:13 +0100

    lsp meckering

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,6 +64,10 @@ return {
 					local buffer = args.buf
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
 
+					if client == nil then
+						return
+					end
+
 					if client.server_capabilities.inlayHintProvider then
 						vim.lsp.inlay_hint.enable(buffer, true)
 					end
