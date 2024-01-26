e51a668bc6bc445aced73f92e6b44f730a8d5fbe
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-01-26 22:12:30 +0100

    use new cmp tab thing from wiki

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -45,12 +45,9 @@ return {
 			local luasnip = require("luasnip")
 
 			local function has_words_before()
-				if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-					return false
-				end
+				unpack = unpack or table.unpack
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
 			local function tab_complete(fallback)
