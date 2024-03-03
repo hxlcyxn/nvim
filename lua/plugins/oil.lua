a54af4531ff0880d3bbc415ad5e29f41d816b34c
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2024-03-03 19:46:48 +0100

    oil settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,9 +4,33 @@ return {
 	"stevearc/oil.nvim",
 	keys = {
 		{ "<leader>-", cmd("Oil"), desc = "open oil" },
+		{
+			"<leader>fo",
+			function()
+				require("oil").toggle_float()
+			end,
+			desc = "open floating oil",
+		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = false,
-	opts = {},
+	opts = {
+		columns = {
+			"icon",
+		},
+		winoptions = {
+			signcolumn = "yes",
+		},
+		float = {
+			padding = 8,
+			border = Settings.border,
+		},
+		preview = {
+			border = Settings.border,
+		},
+		progress = {
+			border = Settings.border,
+		},
+	},
 	config = true,
 }
